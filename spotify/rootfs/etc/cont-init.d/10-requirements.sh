4753f2c955ce36b6155ca0bfa00387a2ff16c238
Franck Nijhof <frenck@addons.community>
2018-07-22 23:09:49 +0200

    :sparkles: Initial add-on code

---
+++ spotify/rootfs/etc/cont-init.d/10-requirements.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: Spotify Connect
+# This files check if all user configuration requirements are met
+# ==============================================================================
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+exit 0
