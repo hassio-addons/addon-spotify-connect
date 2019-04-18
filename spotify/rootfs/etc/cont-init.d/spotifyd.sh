531ef71106d19db16eba9a43e2b828132a07a6cd
Franck Nijhof <frenck@addons.community>
2019-04-18 21:01:32 +0200

    :hammer: Simplifies add-on code logic

--- spotify/rootfs/etc/cont-init.d/spotifyd.sh
+++ spotify/rootfs/etc/cont-init.d/spotifyd.sh
@@ -8,6 +8,21 @@ declare password
 declare name
 declare bitrate
 
+bashio::config.require.username
+bashio::config.require.password
+
+if ! bashio::config.has_value 'name'; then
+    bashio::log.fatal
+    bashio::log.fatal "Add-on configuration is incomplete!"
+    bashio::log.fatal
+    bashio::log.fatal "The Spotify client needs to be identifiable with a name"
+    bashio::log.fatal "and it seems you haven't configured one."
+    bashio::log.fatal
+    bashio::log.fatal "Please set the 'name' add-on option."
+    bashio::log.fatal
+    bashio::exit.nok
+fi
+
 username=$(bashio::config 'username')
 password=$(bashio::config 'password')
 name=$(bashio::config 'name')
